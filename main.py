3fafc3da5fcc8457bdaa4b14fb82ae142eb94c18
Sebastian Garces <segarces23@gmail.com>
2018-11-11 12:45:52 -0500

    Added quit option

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import re
 
 print("Basic Calculator")
+print("Type 'quit' to exit\n")
 
 init = 0
 run = True
 
 
 def perform_math():
+    global run
     equation = input("Enter Equation: ")
-    print("You typed", equation)
+    if equation == "quit":
+        run = False
+    else:
+        print("You typed", equation)
 
 
 while run:
     perform_math()
-
-
