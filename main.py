6e8bd7665f818b1468487afa51334b3370d7e21b
Sebastian Garces <segarces23@gmail.com>
2018-11-11 12:36:16 -0500

    Initial equation input

---
+++ main.py
@@ -0,0 +1,17 @@
+import re
+
+print("Basic Calculator")
+
+init = 0
+run = True
+
+
+def perform_math():
+    equation = input("Enter Equation: ")
+    print("You typed", equation)
+
+
+while run:
+    perform_math()
+
+
